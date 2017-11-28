7f7baed216f1782839080fc3494b9404187ef35c
Jonathan Eberle <mejohnee@users.noreply.github.com>
2017-11-28 20:06:09 +0100

    ich hab da was geloescht, kein plan was das war. sorry!

--- things.R
+++ things.R
@@ -1,29 +1,5 @@
-files <- list.files(path="path/to/dir", pattern="wiki*", full.names=T, recursive=FALSE)
+mydata = read.table("wortschatz/deu_wikipedia_2016_1M-words.txt", sep="\t", quote="", colClasses = c(rep("NULL", 1),rep("character", 1), rep("NULL", 2)))
 
-lapply(files, function(inputFile) {
-  con  <- file(inputFile, open = "r")
-  
-  counter <- 0
-  while (counter < 2000 && length(oneLine <- readLines(con, n = 1, warn = FALSE)) > 0) {
-    print(oneLine)
-    counter <- counter + 1
-  } 
-  print(counter)
-  close(con)
-})
-
-
-
-# inputFile <- "sample.txt"
-# con  <- file(inputFile, open = "r")
-# 
-# counter <- 0
-# while (counter < 2000 && length(oneLine <- readLines(con, n = 1, warn = FALSE)) > 0) {
-#   print(oneLine)
-#   counter <- counter + 1
-# } 
-# print(counter)
-# close(con)
 
 longestCommonSusbtring <- function(a, b) {
   aVec <- unlist(strsplit(a, ""))
