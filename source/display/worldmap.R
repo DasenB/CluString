a8a8d76dec9a05e0848c526ff15971d74d987ba8
trac1337 <tractic@icloud.com>
2018-01-19 17:44:57 +0100

    map

--- source/display/worldmap.R
+++ source/display/worldmap.R
@@ -1,13 +1,37 @@
-#=================
-# INSTALL PACKAGES
-#=================
-library(tidyverse)
-library(rvest)
-library(magrittr)
+# loading the required packages
+library(ggplot2)
 library(ggmap)
-library(stringr)
 
 
-worldmap <- read.csv2("data/wordlist/worldmap.csv", sep=",")
-worldmap <- data.frame(city = worldmap$city, lat=worldmap$lat, lng=worldmap$lng, country=worldmap$country)
-View(worldmap)
+worldmap <<- read.csv("data/wordlist/worldmap.csv")
+worldmap <<- data.frame(city=worldmap$city, lat=worldmap$lat, lng=worldmap$lng)
+
+# hier muss durch jede stadt geloopt
+by(dataFrame, 1:nrow(dataFrame), function(row) dostuff)
+worldmapping <- function(x, output){
+  
+}
+  
+  # lat setzen
+  # lng setzen 
+
+  #plotten der punkte
+
+# distanceMatrix <<- preprocess(wordlist, StoS.TriGram)
+# clusterResult <<- Lloyd(distanceMatrix, recenter.Heuristical, 200)
+
+
+
+# creating a sample data.frame with your lat/lon points
+lon <- c(-38.31,-35.5)
+lat <- c(40.96, 37.5)
+df <- as.data.frame(cbind(lon,lat))
+
+# getting the map
+mapgilbert <- get_map(location = c(lon = mean(df$lon), lat = mean(df$lat)), zoom = 4,
+                      maptype = "satellite", scale = 2)
+
+# plotting the map with some points on it
+ggmap(mapgilbert) +
+  geom_point(data = df, aes(x = lon, y = lat, fill = "red", alpha = 0.8), size = 5, shape = 21) +
+  guides(fill=FALSE, alpha=FALSE, size=FALSE)
