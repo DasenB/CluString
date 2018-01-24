229bad93079504dcbf27d776026857d11681952e
trac1337 <tractic@icloud.com>
2018-01-24 02:57:44 +0100

    pointing in map

--- source/display/worldmap.R
+++ source/display/worldmap.R
@@ -5,8 +5,8 @@
 #pipe <- function() {
 
   germany <<- read.csv2("data/location/GEO-AZ.csv", sep=",", header = FALSE, encoding = "Latin-1")
-  germany <<- data.frame(city = germany$V1, lat=germany$V2, long=germany$V3)
-
+  germany <<- data.frame(city = as.character(germany$V1), lat=germany$V2, lon=germany$V3)
+  View(germany)
   germanyCities <<- germany$city
 
   #distanceMatrix <<- preprocess(wordlist, StoS.TriGram)
@@ -16,16 +16,18 @@
 
   # MAPSECTION
 
-  #cityLat <- germany$lat
-  #cityLong <- germany$long
+  cityLat <- germany$lat
+  cityLon <- germany$lon
 
-  library(ggmap)
 
   Germany.map = get_map(location = "Germany", zoom = 6, color="bw")  ## get MAP data
 
   p <- ggmap(Germany.map)
-
-
+  p <- p + geom_point(data=germany, aes(lat=germany$lat, lon=germany$lon), color=c("red"))
+  p <- p +scale_color_gradient(low = "yellow", high = "red", guide=guide_legend(title = "Price"))
+  p  + theme(axis.title=element_blank(),
+             axis.text=element_blank(),
+             axis.ticks=element_blank()) + ggtitle("All Gas Stations along the Autobahn")
 
 #}
 
