f5b80412a79d61a478e3b2adbcd3fd6b3c2d6804
trac1337 <tractic@icloud.com>
2018-01-24 03:28:32 +0100

    finished city mapping - clustering missing

--- source/display/worldmap.R
+++ source/display/worldmap.R
@@ -5,8 +5,8 @@
 #pipe <- function() {
 
   germany <<- read.csv2("data/location/GEO-AZ.csv", sep=",", header = FALSE, encoding = "Latin-1")
-  germany <<- data.frame(city = as.character(germany$V1), lat=germany$V2, lon=germany$V3)
-  View(germany)
+  germany <<- germany[!is.na(germany$V2) && !is.na(germany$V3) ,]
+  germany <<- data.frame(city = as.character(germany$V1), lat=as.numeric(as.character(germany$V2)), lon=as.numeric(as.character(germany$V3)))
   germanyCities <<- germany$city
 
   #distanceMatrix <<- preprocess(wordlist, StoS.TriGram)
@@ -19,17 +19,17 @@
   cityLat <- germany$lat
   cityLon <- germany$lon
 
+  ggplot(mtcars, aes(mpg, hp)) +
+    geom_point()
 
   Germany.map = get_map(location = "Germany", zoom = 6, color="bw")  ## get MAP data
 
   p <- ggmap(Germany.map)
-  p <- p + geom_point(data=germany, aes(lat=germany$lat, lon=germany$lon), color=c("red"))
-  p <- p +scale_color_gradient(low = "yellow", high = "red", guide=guide_legend(title = "Price"))
+  p <- p + geom_point(data=germany, aes(x=lon, y=lat), color=c("red"))
+  p <- p + scale_color_gradient(low = "yellow", high = "red")
   p  + theme(axis.title=element_blank(),
              axis.text=element_blank(),
-             axis.ticks=element_blank()) + ggtitle("All Gas Stations along the Autobahn")
-
-#}
+             axis.ticks=element_blank()) + ggtitle("Städte Deutschlands im Cluster")
 
 
 
